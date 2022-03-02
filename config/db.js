769b528aa4b9ad02cabbc1d0adb107c8f2cecbc9
maparmigiani <maiaraalmeida92@gmail.com>
2022-03-01 20:01:05 -0500

    something

--- config/db.js
+++ config/db.js
@@ -1,5 +1,6 @@
 module.exports = {
 
-    "URI" : "mongodb://127.0.0.1:27017/contact_information"
+    //"URI" : "mongodb://127.0.0.1:27017/contact_information"
+    "URI": "mongodb+srv://maparmigiani:Gatinha1992@cluster0.dsv1u.mongodb.net/contact_information1?retryWrites=true&w=majority"
 }
 //contact_information
